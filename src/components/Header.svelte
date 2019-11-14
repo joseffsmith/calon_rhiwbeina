111bbcf81afd2a6e0c99de4475602126b957bd18
joseffsmith <joe@rapidtender.com>
2019-11-14 18:21:48 +0000

    Fix text jumping in header and better hover animation

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,25 +10,26 @@
 
 	header { display: flex; padding: 3px; margin-right: 5px; height: 150px; justify-content: space-between; align-items: center; background: #888888; width: 100vw; line-height: 1; }
 	header nav { display: flex; flex-direction: column; flex-grow: 2; }
-	header nav .link { padding: 3px 0 1px 0px; border-bottom: 1px solid #964c67; width: 82px; margin: 2px 2px 4px 2px; }
+	header nav .link { padding: 3px 0px 1px 0px; border-bottom: 1px solid #964c67; width: 82px; margin: 2px 2px 4px 2px; border-radius: 1px; line-height: 20px; }
 
 	header nav .link:hover,
-	header nav .link:focus { background: #fffd; color: #888 }
+	header nav .link:focus { transition: ease-out; transition-duration: .6s; transition-property: background-color; background-color: #964c67; transform-origin: bottom; }
 
 	header nav .link:active, 
-	header nav .link.selected { background: #fffb	; color: #888 }
+	header nav .link.selected { background: #964c67; }
 
 	header nav .social-logo-wrapper { padding: 2px 0 0 4px; }
 	header .right-hand { display: flex; flex-flow: column nowrap; align-items: flex-end; justify-content: space-around; padding-right: 5px; height: 100%; max-width: 60vw; }
 	header .right-hand .address { font-size: 12px; }
-	header .right-hand .address:hover { text-decoration: underline; color:#964c67 }
+	header .right-hand .address:hover { text-decoration: underline; color:#964c67; transition: ease-out; transition-duration: .6s; transition-property: color; transform-origin: bottom; }
 
 	@media screen and (min-width: 750px) {
 		header nav { flex-direction: row; justify-content: space-around; font-size: 20px; }
 	}
 		
 	header img.logo { max-width: 250px; width: 100%; }
-	a       .social-logo { max-width: 25px; min-width:25px;}
+
+	a       .social-logo { max-width: 25px; min-width:25px; max-height: 25px; }
 	a:hover .social-logo { display:none; }
 	a       .social-logo.color { display: none;}
 	a:hover .social-logo.color { display: inline-block; }
